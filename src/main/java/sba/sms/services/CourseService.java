9328babad87ed0d90894d8fa4cb569b3db23b952
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-08-10 21:07:56 -0400

    Update createdCourse method

--- src/main/java/sba/sms/services/CourseService.java
+++ src/main/java/sba/sms/services/CourseService.java
@@ -1,5 +1,7 @@
 package sba.sms.services;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -17,11 +19,29 @@ import java.util.List;
  * provides implementation for each method.
  */
 public class CourseService implements CourseI {
+    private EntityManager entityManager;
 
+    public CourseService(){}
+    public CourseService(EntityManager entityManager){
+        this.entityManager = entityManager;
+    }
 
     @Override
     public void createCourse(Course course) {
+        EntityTransaction transaction = entityManager.getTransaction();
+        try{
+            transaction.begin();
+            entityManager.persist(course);
+            transaction.commit();
+            System.out.println("Course created successfully");
+        }catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
 
+            System.out.println("Error creating course: " + e.getMessage());
+            throw new RuntimeException("Failed to create course", e);
+        }
     }
 
     @Override
