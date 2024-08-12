a7e8490e1f517a1e5baaec290dfc70e5138e9c61
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-08-12 11:57:50 -0400

    Modified all the class methods

--- src/main/java/sba/sms/services/StudentService.java
+++ src/main/java/sba/sms/services/StudentService.java
@@ -1,17 +1,15 @@
 package sba.sms.services;
 
-
-import lombok.extern.java.Log;
-import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 import sba.sms.dao.StudentI;
 import sba.sms.models.Course;
 import sba.sms.models.Student;
-import sba.sms.utils.HibernateUtil;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 /**
@@ -20,164 +18,144 @@ import java.util.List;
  * provides implementation for each method. Lombok @Log used to
  * generate a logger file.
  */
-@Log
+
 public class StudentService implements StudentI {
 
+    SessionFactory factory = new Configuration().configure().buildSessionFactory();
+    Session session = null;
 
-    @Override
-    public List<Student> getAllStudents() {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Transaction transaction = null;
-        List<Student> students = null;
-        try {
-            transaction = session.beginTransaction();
-            String hq1 = "FROM Student";
-            Query<Student> query = session.createQuery(hq1, Student.class);
-            students = query.getResultList();
-            transaction.commit();
-            log.info("Students retrieved successfully");
-        } catch (HibernateException e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-
-            log.severe("Error retrieving students: " + e.getMessage());
-            throw new RuntimeException("Failed to retrieve students", e);
-        }
-        return students;
-    }
-
-    @Override
-    public void createStudent(Student student) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
+    public void createStudent(Student newStudent) {
         Transaction transaction = null;
 
         try {
+
+            //Opening session and beginning the transaction
+            session = factory.openSession();
             transaction = session.beginTransaction();
-            session.persist(student);
+
+            session.persist(newStudent);
             transaction.commit();
-            log.info("Students retrieved successfully");
-        } catch (HibernateException e) {
+
+        } catch (Exception e) {
             if (transaction != null) {
                 transaction.rollback();
+                System.out.println(e.getMessage());
             }
-
-            log.severe("Error creating students: " + e.getMessage());
-            throw new RuntimeException("Failed to create students", e);
-        } finally {
-            session.close();
+            System.out.println(e.getMessage());
         }
     }
 
-    @Override
     public Student getStudentByEmail(String email) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Transaction transaction = null;
+
         Student student = null;
+        Transaction transaction = null;
         try {
+
+            session = factory.openSession();
             transaction = session.beginTransaction();
-            String hq2 = "FROM Student WHERE email = :email";
-            Query<Student> query = session.createQuery(hq2, Student.class);
-            student = query.uniqueResult();
-            transaction.commit();
-            log.info("Students retrieved successfully");
-        } catch (HibernateException e) {
+            student = session.get(Student.class, email);
+
+        } catch (Exception e) {
             if (transaction != null) {
                 transaction.rollback();
+                System.out.println(e.getMessage());
             }
-
-            log.severe("Error retrieving students: " + e.getMessage());
-            throw new RuntimeException("Failed to retrieve students", e);
-        } finally {
-            session.close();
+            System.out.println(e.getMessage());
         }
         return student;
     }
 
-    @Override
     public boolean validateStudent(String email, String password) {
 
-        Session session = HibernateUtil.getSessionFactory().openSession();
         Transaction transaction = null;
-        boolean isValid = false;
+
         try {
+            session = factory.openSession();
             transaction = session.beginTransaction();
-            String hq3 = "FROM Student WHERE email = :email AND password = :password";
-            Query<Student> query = session.createQuery(hq3, Student.class);
+
+
+            String hql = "FROM  Student WHERE email = :email AND password = :password";
+            Query<Student> query = session.createQuery(hql, Student.class);
+
             query.setParameter("email", email);
             query.setParameter("password", password);
+            Student student = query.getSingleResult();
             transaction.commit();
-            log.info("Student validation completed");
-        } catch (HibernateException e) {
+
+            return student != null;
+
+        } catch (Exception e) {
             if (transaction != null) {
                 transaction.rollback();
+                System.out.println(e.getMessage());
             }
-
-            log.severe("Error validating student: " + e.getMessage());
-            throw new RuntimeException("Failed to validate student", e);
-        } finally {
-            session.close();
+            System.out.println(e.getMessage());
         }
-        return isValid;
+
+        return false;
+
     }
 
-    @Override
-    public void registerStudentToCourse(String email, int courseId) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
+    public List<Student> getAllStudents() {
         Transaction transaction = null;
-        try {
-            transaction = session.beginTransaction();
-            String hq4 = "FROM Student WHERE email = :email";
-            Student student = session.createQuery(hq4, Student.class).setParameter("email", email).uniqueResult();
-            Course course = session.get(Course.class, courseId);
-            if (student != null && course != null) {
-                student.getCourses().add(course);
-                session.update(student);
-                transaction.commit();
-                log.info("Student registered to course successfully");
-            } else {
-                log.warning("Student or Course not found");
 
-            }
-        } catch (HibernateException e) {
+        try {
+            String hqlString = "SELECT Student from Students";
+            Query<Student> query = session.createQuery(hqlString, Student.class);
+            return query.getResultList();
+        } catch (Exception e) {
             if (transaction != null) {
                 transaction.rollback();
+                System.out.println(e);
             }
-
-            log.severe("Error registering student to course: " + e.getMessage());
-            throw new RuntimeException("Failed to register student to course", e);
-        } finally {
-            session.close();
+            System.out.println(e);
+            return null;
         }
     }
 
-    @Override
     public List<Course> getStudentCourses(String email) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
         Transaction transaction = null;
-        List<Course> courses = null;
+        try {
+
+            transaction = session.beginTransaction();
+
+            //String to query all te courses for the students
+            String hql = "SELECT course FROM Course course JOIN course.students student WHERE student.email = :email";
+
+            Query<Course> query = session.createQuery(hql, Course.class);
+            query.setParameter("email", email);
+            List<Course> StudentCourses = query.getResultList();
+            transaction.commit();
+            return StudentCourses;
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+                System.out.println(e.getMessage());
+            }
+            System.out.println(e.getMessage());
+            return null;
+        }
+
+    }
+
+    public void registerStudentToCourse(String email, int courseId) {
+        Transaction transaction = null;
+        session = factory.openSession();
+
         try {
             transaction = session.beginTransaction();
-            String hq4 = "FROM Student WHERE email = :email";
-            Student student = session.createQuery(hq4, Student.class).setParameter("email", email).uniqueResult();
-            if (student != null) {
-                courses = new ArrayList<>(student.getCourses());
-            } else {
-                log.warning("Student not found for email" + email);
-
-            }
+            Student student = session.get(Student.class, email);
+            Course course = session.get(Course.class, courseId);
+            student.getCourses().add(course);
+            session.merge(student);
             transaction.commit();
-            log.info("Courses for student retrieved successfully");
-        } catch (HibernateException e) {
+        } catch (Exception e) {
             if (transaction != null) {
                 transaction.rollback();
             }
-
-            log.severe("Error retrieving student courses " + e.getMessage());
-            throw new RuntimeException("Failed to retrieve student courses", e);
-        } finally {
-            session.close();
+            System.out.println(e.getMessage());
         }
-        return courses;
     }
+
 }
