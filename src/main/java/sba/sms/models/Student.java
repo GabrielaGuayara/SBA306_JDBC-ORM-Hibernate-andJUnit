2200b912a7cc46adbd04ffd51de26980e919458e
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-08-12 21:50:48 -0400

    Applied lombok annotations to minimize code

--- src/main/java/sba/sms/models/Student.java
+++ src/main/java/sba/sms/models/Student.java
@@ -2,18 +2,7 @@ package sba.sms.models;
 
 import jakarta.persistence.*;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
-
-
-/**
- * Student is a POJO, configured as a persistent class that represents (or maps to) a table
- * name 'student' in the database. A Student object contains fields that represent student
- * login credentials and a join table containing a registered student's email and course(s)
- * data. The Student class can be viewed as the owner of the bi-directional relationship.
- * Implement Lombok annotations to eliminate boilerplate code.
- */
-import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
@@ -66,7 +55,7 @@ public class Student {
     }
 
     public String getName() {
-        return this.name;
+        return name;
     }
 
     public String getPassword() {
